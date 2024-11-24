245a4350b1fa8ce47fab615810f46185e17a84d2
Rebecca <86becca09@gmail.com>
2024-11-25 01:49:25 +0800

    searchBtns need to be combined

--- ChiikawaGame/src/main/java/com/example/demo/model/SaInfoRepository.java
+++
@@ -1,9 +0,0 @@
-//(Mantle)
-package com.example.demo.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaInfoRepository extends JpaRepository<SaInfo, Integer> {
-}
